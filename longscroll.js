306a08735b370c46d53ea3bd992f946f95218eaa
Marc V <mike@Michaels-MacBook-Pro-2.local>
2017-06-10 20:01:43 -0700

    marc edits 4.1
    1. resized airplane, cake, clothesfront
    2. added divs for cake, airplane
    3.replaced bg

--- longscroll.js
+++ longscroll.js
@@ -13,31 +13,30 @@ $(document).ready(function(){
   
     
     if(fromTop > 2000 ){
-       // if we have scrolled MORE than 2000 pixels use this image
-      
+    
       $("#fallingphil").css({
            "background" : "url('img/phil1.5.png')"
            });
       
-      
-    } else if(fromTop > 5100){
+    }
     
+  else if(fromTop > 5100){
      $("#fallingphil").css({
            "background" : "url('img/phil2.png')"
            });
     
     }
+    
+    else if(fromTop > 7700){
+     $("#fallingphil").css({
+           "background" : "url('img/phil3.png')"
+           });
+    
+    }
     else {
-      // else if we have scrolled LESS than 3000 pixels use this image
-      
-      //$("#wrapper").css({"border":"1px solid black"});
-      //$("body").css({"background-color":"white"});
-      
       $("#fallingphil").css({
          "background" : "url('img/phil.png')"
        });
-     // $("#fallingphil").text("https://jsbin.com/yibujax/3/edit?js,output");
-      
       
     }//end if statement
     
